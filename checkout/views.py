5629687ba11950e41a1f9f9715ecf633c31bf2e7
Kayleigh Ellis <elliskay@Kayleighs-iMac.local>
2022-04-28 21:25:48 +0100

    build out checkout page design and structure

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import OrderForm
 
 # Create your views here.
+
+#checks if bag is empty before directing to checkout if not triggers a toast error message
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Oops! There's nothing in your bag right now")
+        return redirect('products')
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+    
+    return render(request, template, context)
