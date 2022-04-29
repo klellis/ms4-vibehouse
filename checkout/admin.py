320f1cbef2b2c6d752e713acda272cb8a9dac657
klellis <kl_ellis@hotmail.co.uk>
2022-04-29 21:42:20 +0000

    add product functionality for superuser

--- checkout/admin.py
+++ checkout/admin.py
@@ -16,9 +16,8 @@ class OrderAdmin(admin.ModelAdmin):
                        'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
-              'email', 'phone_number',
-              'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'delivery_cost',
+              'email', 'postcode','town_or_city', 'street_address1',
+              'street_address2', 'delivery_cost',
               'order_total', 'grand_total', 'original_bag',
               'stripe_pid')
 
