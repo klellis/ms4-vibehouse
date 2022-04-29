4395451fcd3fe90b0b5c693b90594fa34ccab2b0
klellis <kl_ellis@hotmail.co.uk>
2022-04-29 11:03:47 +0000

    set payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,9 @@ import stripe
 
 # checks if bag is empty before directing to checkout if not triggers a toast error message
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Oops! There's nothing in your bag right now")
@@ -18,12 +21,19 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
     
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KthVfApZqtZnCG8uvvR3NKIyqP6fTYb5mjDryVMxXZF7RG0oErNr2yjIdG6QNY0xoS3MxhHp9nIbQaeQvqXLt7x00mNU9RYPr',
-        'client_secret': 'test secret key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, template, context)
